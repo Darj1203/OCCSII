f67d28463503dd38661605267a3ecbc38c20a416
Darj1203 <darj1203@gmail.com>
2023-11-19 13:52:33 -0500

    Se solucionan errores en el codigo

--- BK-OCCS/app/routes/apartamentos.routes.js
+++ BK-OCCS/app/routes/apartamentos.routes.js
@@ -1,12 +1,14 @@
 import apartamentosController from '../controllers/apartamentos.controller.js';
-import Router from 'express';
+import express from 'express';
 
-const apartamentosRouter = Router(); 
+const router = express.Router(); 
 
 // Crear nuestras rutas
-apartamentosRouter.get('/', apartamentosController.Listar);
-apartamentosRouter.get('/:id', apartamentosController.BuscarUno);
-apartamentosRouter.post('/', apartamentosController.CrearApartamento);
-apartamentosRouter.delete('/:id', apartamentosController.EliminarApartamento);
+router.get('/', apartamentosController.BuscarTodos);
+router.get('/:id', apartamentosController.BuscarUno);
+router.post('/', apartamentosController.CrearApartamento);
+router.put('/:id', apartamentosController.ActualizarApartamento);
+router.delete('/:id', apartamentosController.EliminarApartamento);
 
-export default apartamentosRouter;
+
+export default router;
