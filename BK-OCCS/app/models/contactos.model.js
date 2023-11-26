2af787e36752dcb154b6abacd622a68455df10a2
TattanLopez <tattanmartinez3@gmail.com>
2023-11-26 14:31:49 -0500

    se agrego id en contacto
    id

--- BK-OCCS/app/models/contactos.model.js
+++ BK-OCCS/app/models/contactos.model.js
@@ -1,5 +1,11 @@
 export default (config, DataTypes, ApartamentoModel) => {
     const ContactosModel = config.define("Contactos", {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        
         nombre: {
             type: DataTypes.STRING,
             allowNull: false
--- BK-OCCS/app/routes/usuarios.routes.js
+++ BK-OCCS/app/routes/usuarios.routes.js
@@ -4,8 +4,8 @@ import express from 'express';
 const router = express.Router(); 
 
 // Crear nuestras rutas
-router.get('/autorizar', usuariosController.generarTokenYCookie);
-router.get('/verificar', usuariosController.verificarToken);
+router.post('/autorizar', usuariosController.generarTokenYCookie);
+router.post('/verificar', usuariosController.verificarToken);
 router.post('/cerrarsesion', usuariosController.cerrarSesion);
 router.post('/crearusuario', usuariosController.CrearUsuario);
 export default router;
